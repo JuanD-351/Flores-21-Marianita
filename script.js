6a6e2383ac3f79ff176d23883e323d4d038fade1
JuanD-351 <koalaextreme351@gmail.com>
2025-09-21 10:39:27 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 onload = () => {
   document.body.classList.remove("container");
 };
+
+function toggleMessage() {
+  const overlay = document.getElementById("messageOverlay");
+  overlay.style.display = overlay.style.display === "flex" ? "none" : "flex";
+}
